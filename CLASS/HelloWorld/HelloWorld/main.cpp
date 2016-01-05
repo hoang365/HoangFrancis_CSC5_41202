088fb625566f6f98aa951b83180111f2fa14880b
hoang365 <hoang365@yahoo.com>
2016-01-05 10:48:23 -0800

    Upload my programming online

---
+++ CLASS/HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,34 @@
+/* 
+     File:   main.cpp
+     Author: Francis Hoang
+     Created on January 4, 2016, 10:18 AM
+     Purpose: Check out IDE
+ */
+//System Libraries
+#include <iostream>
+
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function prototypes
+
+//Execution begins here
+
+ 
+
+int main(int argc, char** argv) {
+    //Declare and initialize variables
+    
+    //Input data
+    
+    //Calculate or map inputs to outputs
+    
+    //Output the results
+    cout<<"Hello World"<<endl;
+    //Exit stage right
+    return 0;
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # HoangFrancis_CSC5_41202
+RCC Winter CSC 5 Intro to Programming
