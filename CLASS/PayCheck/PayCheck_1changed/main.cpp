ff5870b9aa908a155f86c5e4f4d5e59443406aba
hoang365 <hoang365@yahoo.com>
2016-01-05 11:42:49 -0800

    Signed-off-by: hoang365 <hoang365@yahoo.com>

---
+++ CLASS/PayCheck/PayCheck_1changed/main.cpp
@@ -0,0 +1,38 @@
+/* 
+     Author: Francis Hoang
+     Created on January 5, 2016, 9:30 AM
+     Purpose: Calculate Paycheck, Straight Time
+ */
+//System Libraries
+#include <iostream>
+
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function prototypes
+
+//Execution begins here
+
+ 
+
+int main(int argc, char** argv) {
+    //Declare and initialize variables
+    unsigned char hrsWrkd = 50; //hours worked (hrs)
+    float payRate = 2e1f;  // Pay Rate in $
+    
+    //Input data
+    
+    //Calculate or map inputs to outputs
+    float payChk = payRate*hrsWrkd;
+    //Output the results
+    cout<<"Pay Rate     = $"<<payRate<<"'s/hr"<<endl;
+    cout<<"Hours Worked = "<<static_cast<int>(hrsWrkd)<<" hrs"<<endl;
+    cout<<"Pay Check    = $"<<payChk <<endl;
+    
+    //Exit stage right
+    return 0;
+}
+
---
+++ CLASS/PayCheck/PayCheck_1changed/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=Cygwin_4.x-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
+CND_ARTIFACT_NAME_Debug=paycheck_1changed
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/paycheck_1changed
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
+CND_PACKAGE_NAME_Debug=paycheck1changed.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/paycheck1changed.tar
+# Release configuration
+CND_PLATFORM_Release=Cygwin_4.x-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
+CND_ARTIFACT_NAME_Release=paycheck_1changed
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/paycheck_1changed
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
+CND_PACKAGE_NAME_Release=paycheck1changed.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/paycheck1changed.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
