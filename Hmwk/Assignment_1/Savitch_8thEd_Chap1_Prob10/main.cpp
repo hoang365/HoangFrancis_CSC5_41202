88284f546736887befd4ed6374f7ca3691dc5e6c
hoang365 <hoang365@yahoo.com>
2016-01-06 10:14:12 -0800

    Signed-off-by: hoang365 <hoang365@yahoo.com>

---
+++ Hmwk/Assignment_1/Savitch_8thEd_Chap1_Prob10/main.cpp
@@ -0,0 +1,36 @@
+/* 
+    Author: Francis Hoang
+    Created on January 6, 2016, 10:13 AM
+    Purpose:  Big C
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare and initialize variables
+    //char letter='C';
+    char letter=67;
+    
+    //Output the results
+    cout<<"  "<<letter<<letter<<letter<<endl;
+    cout<<" "<<letter<<"   "<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<" "<<letter<<"   "<<letter<<endl;
+    cout<<"  "<<letter<<letter<<letter<<endl;
+    
+    //Exit stage right
+    return 0;
+}
