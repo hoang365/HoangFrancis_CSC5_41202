872842ba8c85c6b6bad026433543b1429cfb8e98
hoang365 <hoang365@yahoo.com>
2016-01-05 22:22:48 -0800

    hello World assignment

---
+++ Assignments/helloWorld/main.cpp
@@ -0,0 +1,21 @@
+
+
+/* 
+ * 
+ * Author: Francis
+ * Purpose: Make sure Netbeans works
+ * Created on January 5, 2016, 10:03 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout<<"Hello World!!!"<<endl; // says Hello to the world
+    return 0;
+}
+
