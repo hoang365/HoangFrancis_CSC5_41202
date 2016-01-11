d59fab7345110a7f3a9649e7e59f8cf1413f2b27
hoang365 <hoang365@yahoo.com>
2016-01-11 11:07:18 -0800

    Signed-off-by: hoang365 <hoang365@yahoo.com>

--- CLASS/Military_Budget_Calculation/Military_Budget_Calculation_1_V2/nbproject/Makefile-variables.mk
+++ CLASS/Military_Budget_Calculation/Military_Budget_Calculation_1_V2/nbproject/Makefile-variables.mk
@@ -7,21 +7,21 @@ CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
 # Debug configuration
-CND_PLATFORM_Debug=Cygwin-Windows
-CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
+CND_PLATFORM_Debug=Cygwin_4.x-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
 CND_ARTIFACT_NAME_Debug=military_budget_calculation_1_v2
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/military_budget_calculation_1_v2
-CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/military_budget_calculation_1_v2
+CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
 CND_PACKAGE_NAME_Debug=militarybudgetcalculation1v2.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/militarybudgetcalculation1v2.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/militarybudgetcalculation1v2.tar
 # Release configuration
-CND_PLATFORM_Release=Cygwin-Windows
-CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
+CND_PLATFORM_Release=Cygwin_4.x-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
 CND_ARTIFACT_NAME_Release=military_budget_calculation_1_v2
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/military_budget_calculation_1_v2
-CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/military_budget_calculation_1_v2
+CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
 CND_PACKAGE_NAME_Release=militarybudgetcalculation1v2.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/militarybudgetcalculation1v2.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/militarybudgetcalculation1v2.tar
 #
 # include compiler specific variables
 #
