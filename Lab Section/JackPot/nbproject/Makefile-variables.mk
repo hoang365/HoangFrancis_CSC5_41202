90dd3a540a8626efdb3fddefb32961569e910d6f
hoang365 <hoang365@yahoo.com>
2016-01-11 12:12:06 -0800

    Signed-off-by: hoang365 <hoang365@yahoo.com>

--- Lab Section/JackPot/nbproject/Makefile-variables.mk
+++ Lab Section/JackPot/nbproject/Makefile-variables.mk
@@ -9,19 +9,19 @@ CND_DISTDIR=dist
 # Debug configuration
 CND_PLATFORM_Debug=Cygwin_4.x-Windows
 CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin_4.x-Windows
-CND_ARTIFACT_NAME_Debug=truthtable
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/truthtable
+CND_ARTIFACT_NAME_Debug=jackpot
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/jackpot
 CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin_4.x-Windows/package
-CND_PACKAGE_NAME_Debug=truthtable.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/truthtable.tar
+CND_PACKAGE_NAME_Debug=jackpot.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin_4.x-Windows/package/jackpot.tar
 # Release configuration
 CND_PLATFORM_Release=Cygwin_4.x-Windows
 CND_ARTIFACT_DIR_Release=dist/Release/Cygwin_4.x-Windows
-CND_ARTIFACT_NAME_Release=truthtable
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/truthtable
+CND_ARTIFACT_NAME_Release=jackpot
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin_4.x-Windows/jackpot
 CND_PACKAGE_DIR_Release=dist/Release/Cygwin_4.x-Windows/package
-CND_PACKAGE_NAME_Release=truthtable.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/truthtable.tar
+CND_PACKAGE_NAME_Release=jackpot.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin_4.x-Windows/package/jackpot.tar
 #
 # include compiler specific variables
 #
