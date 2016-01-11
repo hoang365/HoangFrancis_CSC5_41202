d7085a4a157ebf57a600914999c6521e0cb9ec52
hoang365 <hoang365@yahoo.com>
2016-01-11 00:01:20 -0800

    Signed-off-by: hoang365 <hoang365@yahoo.com>

--- Hmwk/Assignment_1/Savitch_8thEd_Chap1_Prob10/main.cpp
+++ Hmwk/Assignment_1/Savitch_8thEd_Chap1_Prob10/main.cpp
@@ -17,25 +17,19 @@ using namespace std;
 //Execution Begins Here
 int main(int argc, char** argv) {
     //Declare and initialize variables
-    
+    //char letter='C';
+    char letter=67;
     
     //Output the results
-    cout<<"********************************************"<<endl;
-    cout<<endl;
-    cout<<endl;
-    cout<<"      C C C              S S S S       !!"<<endl;
-    cout<<"     C     C            S       S      !!"<<endl;
-    cout<<"    C                  S               !!"<<endl;
-    cout<<"   C                    S              !!"<<endl;
-    cout<<"   C                     S S S S       !!"<<endl; 
-    cout<<"   C                            S      !!"<<endl;
-    cout<<"    C                            S     !!"<<endl;
-    cout<<"     C     C            S       S        "<<endl;
-    cout<<"      C C C              S S S S       00"<<endl;
-    cout<<endl;
-    cout<<endl;
-    cout<<"********************************************"<<endl;
-    cout<<"   Computer Science is Cool Stuff!!!"<<endl;
+    cout<<"  "<<letter<<letter<<letter<<endl;
+    cout<<" "<<letter<<"   "<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<letter<<endl;
+    cout<<" "<<letter<<"   "<<letter<<endl;
+    cout<<"  "<<letter<<letter<<letter<<endl;
     
     //Exit stage right
     return 0;
--- Hmwk/Assignment_1/Savitch_9thEd_Chap1_Prob2_Program_Proj/main.cpp
+++ Hmwk/Assignment_1/Savitch_9thEd_Chap1_Prob2_Program_Proj/main.cpp
@@ -21,7 +21,22 @@ using namespace std;
 
 int main(int argc, char** argv) {
     //Declare and initialize variables
-   
+   cout<<"********************************************"<<endl;
+    cout<<endl;
+    cout<<endl;
+    cout<<"      C C C              S S S S       !!"<<endl;
+    cout<<"     C     C            S       S      !!"<<endl;
+    cout<<"    C                  S               !!"<<endl;
+    cout<<"   C                    S              !!"<<endl;
+    cout<<"   C                     S S S S       !!"<<endl; 
+    cout<<"   C                            S      !!"<<endl;
+    cout<<"    C                            S     !!"<<endl;
+    cout<<"     C     C            S       S        "<<endl;
+    cout<<"      C C C              S S S S       00"<<endl;
+    cout<<endl;
+    cout<<endl;
+    cout<<"********************************************"<<endl;
+    cout<<"   Computer Science is Cool Stuff!!!"<<endl;
     
    
    
