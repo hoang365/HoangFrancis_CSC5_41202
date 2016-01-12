ece10973e2f30fce2fd5f83cb919ba8edff12020
hoang365 <hoang365@yahoo.com>
2016-01-12 10:13:06 -0800

    Signed-off-by: hoang365 <hoang365@yahoo.com>

---
+++ Hmwk/Assignment_2/Savitch_9thEd_Practice_Prob4/main.cpp
@@ -0,0 +1,50 @@
+/* 
+     File:   main.cpp
+     Author: Francis Hoang
+     Created on January 12, 2016, 10:02 AM
+     Purpose: Mad Libs
+ */
+//System Libraries
+#include <iostream>
+#include <string>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function prototypes
+
+//Execution begins here
+
+ 
+
+int main(int argc, char** argv) {
+    //Declare and initialize variables
+    string insName, name, food, adjec, color, animal;
+    int number;
+    //Calculate or map inputs to outputs
+    cout<<"Enter your instructor name: ";
+    cin>>insName;
+    cout<<"Enter your name: ";
+    cin>>name;
+    cout<<"Enter a food: ";
+    cin>>food;
+    cout<<"Enter a number between 100 and 120: ";
+    cin>>number;
+    cout<<"Enter an adjective: ";
+    cin>>adjec;
+    cout<<"Enter a color: ";
+    cin>>coor;
+    cout<<"Enter an animal: ";
+    cin>>animal;
+    
+   
+    
+    
+    //Output the results
+    
+    //Exit stage right
+    return 0;
+}
+
