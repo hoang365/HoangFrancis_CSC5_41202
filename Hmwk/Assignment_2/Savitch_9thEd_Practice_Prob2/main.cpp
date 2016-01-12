cc08db5de7aa3113c171993d6ba30565aaa5e272
hoang365 <hoang365@yahoo.com>
2016-01-12 08:43:05 -0800

    Signed-off-by: hoang365 <hoang365@yahoo.com>

---
+++ Hmwk/Assignment_2/Savitch_9thEd_Practice_Prob2/main.cpp
@@ -0,0 +1,45 @@
+/* 
+     File:   main.cpp
+     Author: Francis Hoang
+     Created on January 12, 2016, 8:32 AM
+     Purpose: Babylonian square root
+ */
+//System Libraries
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function prototypes
+
+//Execution begins here
+
+ 
+
+int main(int argc, char** argv) {
+    //Declare and initialize variables
+    float guess; //the initial guess
+    float r;   //square root
+    int n;     //number
+    int count = 0; //number of times ran
+    //Calculate or map inputs to outputs
+    //cout<<fixed<<setprecision(2)<<showpoint;
+    cout<<"Enter the number to find the square root of: ";
+    cin>>n;
+    guess = n/2;
+    
+    while (count != 100)
+    {
+        count+=1;
+        r = n/guess;
+        guess = (guess+r)/2;
+    }
+    //Output the results
+    cout<<"The Babylonians believe the square root to be "<<r<<endl;
+    //Exit stage right
+    return 0;
+}
+
