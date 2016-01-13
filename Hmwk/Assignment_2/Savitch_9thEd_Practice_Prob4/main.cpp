67912a2f9edb9a14dc8388f9ae7bbfac8dd1ba07
hoang365 <hoang365@ahoo.com>
2016-01-13 07:52:07 -0800

    Signed-off-by: hoang365 <hoang365@ahoo.com>

--- Hmwk/Assignment_2/Savitch_9thEd_Practice_Prob4/main.cpp
+++ Hmwk/Assignment_2/Savitch_9thEd_Practice_Prob4/main.cpp
@@ -32,10 +32,14 @@ int main(int argc, char** argv) {
     cin>>food;
     cout<<"Enter a number between 100 and 120: ";
     cin>>number;
+    while(number <100 || number > 120){
+        cout<<"Please enter a number between 100 and 120: ";
+        cin>>number;
+    }
     cout<<"Enter an adjective: ";
     cin>>adjec;
     cout<<"Enter a color: ";
-    cin>>coor;
+    cin>>color;
     cout<<"Enter an animal: ";
     cin>>animal;
     
@@ -43,7 +47,15 @@ int main(int argc, char** argv) {
     
     
     //Output the results
-    
+    cout<<"Dear Instructor "<<insName<<","<<endl<<endl;
+    cout<<"I am sorry that I am unable to turn in my homework at this time. First,"<<endl;
+    cout<<"I ate a rotten "<<food<<", which made me turn "<<color<<" and extremely ill. I"<<endl;
+    cout<<"came down wih a fever of "<<number<<". Next, my "<<adjec<<" pet"<<endl;
+    cout<<animal<<" must have smelled the remains of the "<<food<<" on my homework,"<<endl;
+    cout<<"because he ate it. I am currently rewriting my homework and hope you"<<endl;
+    cout<<"will accept it late."<<endl<<endl;
+    cout<<"Sincerely,"<<endl;
+    cout<<name<<endl;
     //Exit stage right
     return 0;
 }
